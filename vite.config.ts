4160e660990052f46f4cf04b5aa25c639d850e9a
bastian diaz <basty.diaaz@gmail.com>
2025-09-26 11:20:32 -0300

    chore: install tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,5 +14,6 @@ export default defineConfig({
 				previewMiddlewareFallback: '/404',
 			},
 		}),
+		tailwindcss(),
 	],
 });
